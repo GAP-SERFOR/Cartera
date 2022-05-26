f921df187b4e1d189bb4ac86ae8dfddeca3fdd0e
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2022-05-26 16:20:35 -0500

    Delete js directory

--- js/botones/script.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector("#menu-bars");
-let navbar = document.querySelector(".navbar__items");
-
-menu.onclick = () => {
-    menu.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-}
-
